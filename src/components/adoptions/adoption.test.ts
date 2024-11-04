4d4a1aebb8678ba0962a4c45041c9efe7927074d
josedejesusAmaya <hello.devpp@gmail.com>
2024-11-04 00:13:57 -0600

    Added seed for tests

--- src/components/adoptions/adoption.test.ts
+++ src/components/adoptions/adoption.test.ts
@@ -1,5 +1,5 @@
-//import request from "supertest";
-//import app from "../../app";
+import request from "supertest";
+import app from "../../app";
 import testDatabase from "../../config/test.database.config";
 import { AdoptionRecord, IAdoption } from "./adoption.model";
 
@@ -7,7 +7,19 @@ beforeAll(async () => {
   await testDatabase.sync({ force: true }); // Reset the database before each test run
   const seed: IAdoption[] = [
     {
-     // TODO:
+      id: 1,
+      name: "",
+      email: "",
+      phone: 0,
+      street: "",
+      streetNumber: 0,
+      city: "",
+      state: "",
+      zip: 0,
+      country: "",
+      petId: 0,
+      notes: "",
+      status: "Review"
     },
   ];
   await AdoptionRecord.bulkCreate(seed);
@@ -17,4 +29,6 @@ afterAll(async () => {
   await testDatabase.close(); // Close the database connection after all tests
 });
 
-// TODO
+describe('Adoption', () => {
+  describe();
+});
